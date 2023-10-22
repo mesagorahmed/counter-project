c0f09281663cadc7a3f3075b32421bf09cf5eca2
Sagor Ahmed <iamsagorahmed@gmail.com>
2023-10-22 10:03:47 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const myCount = document.querySelector('#count');
+const btns = document.querySelectorAll('.btn');
+
+let count = 0;
+btns.forEach((btn) => {
+
+    btn.addEventListener('click', (e) => {
+        
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+            
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+             count = 0;
+        }
+        myCount.textContent = count;        
+    });   
+    
+});
+
